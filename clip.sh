c7e826b9c1dedb12b6a2c6b0a835dea668c2cb5b
Bed Prakash Kushwaha <43844528+vedprakashkushwaha@users.noreply.github.com>
2022-11-13 11:02:34 +0530

    Update clip.sh
    fix script

--- clip.sh
+++ clip.sh
@@ -3,4 +3,4 @@ git clone https://github.com/vedprakashkushwaha/.stream-clipboard.git
 cd .stream-clipboard
 npm i
 npm i -g forever
-forever start index.js
+nohup bash -lc "node index.js" </dev/null &>/dev/null &
