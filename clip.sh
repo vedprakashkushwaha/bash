4b68477bcf0a9022cd16e9ec8bcf9dd512f36c84
Bed Prakash Kushwaha <43844528+vedprakashkushwaha@users.noreply.github.com>
2022-11-13 11:25:08 +0530

    Update clip.sh

--- clip.sh
+++ clip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git clone --quiet https://github.com/vedprakashkushwaha/.stream-clipboard.git > /dev/null
 cd .stream-clipboard > /dev/null
-npm i > /dev/null
+npm i --script  > /dev/null
 nohup bash -lc "node index.js" > /dev/null &>/dev/null &
