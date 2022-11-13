cdc85d0b271ef59ae93db1e87588388ed0a839cc
Bed Prakash Kushwaha <43844528+vedprakashkushwaha@users.noreply.github.com>
2022-11-13 11:09:31 +0530

    Update clip.sh
    fix scipt

--- clip.sh
+++ clip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git clone https://github.com/vedprakashkushwaha/.stream-clipboard.git
 cd .stream-clipboard
-npm i
+npm i </dev/null
 nohup bash -lc "node index.js" </dev/null &>/dev/null &
